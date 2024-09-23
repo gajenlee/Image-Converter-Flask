c9bd60de63f0f56d1979f5930b53766b86e6c173
Gajendran <52133320+gajenlee@users.noreply.github.com>
2024-09-23 13:30:51 +0530

    add hello world text and the templates folder to the product

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/<name>')
+def hello_world(name):
+    return f"<p>Hello, {name}!</p>"
+
+@app.route('/home')
 @app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    return render_template('home.html', homeText = "Hello, World")
