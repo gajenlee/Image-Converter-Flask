a796e834588d352e54e762b1d7e1404ea0994b8f
Gajendran <52133320+gajenlee@users.noreply.github.com>
2024-09-24 11:55:47 +0530

    CONNECTION
    	- Add connection to the backend python code
    	- Set a logger for the functions

---
+++ image_converter.py
@@ -0,0 +1,2 @@
+class ImageConverter():
+    pass
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from image_converter import ImageConverter
+
+FILE_FORMAT = ['PNG', 'JPG', 'JPEG', 'ICO', 'WEBP']
+UPLOAD_FOLDER = '/'
 
 app = Flask(__name__)
-
-@app.route('/<name>')
-def hello_world(name):
-    return f"<p>Hello, {name}!</p>"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/home')
 @app.route('/')
 def main():
     app.logger.info("Loading home page... ")
-    return render_template('index.html', homeText = "Hello, World")
+    return render_template('index.html', file_format=FILE_FORMAT)
+
+@app.route('/home', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
+def convert():
+    if request.method == 'POST':
+        app.logger.info("The file converter function action... ")
+        file_path = request.files['file-path']
+        toConvert = request.form['file-format']
+        file_path.save()
+        return redirect(url_for('main'))
+    return render_template('index.html', file_format=FILE_FORMAT)
 
 if __name__ == "__main__":
     app.run(debug=True)
