aaf2f81063b1872bf17b90b7dd1f4820d6181d46
Gajendran <52133320+gajenlee@users.noreply.github.com>
2024-09-24 13:30:04 +0530

    FILE HANDLING
    	- Add download function to the use able to download the file

--- image_converter.py
+++ image_converter.py
@@ -1,2 +1,8 @@
 class ImageConverter():
-    pass
+    
+    def __init__(self, file_path, convert_type):
+        self.file_path = file_path
+        self.convert_type = convert_type
+    
+    def convert(self):
+        return self.file_path
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 from image_converter import ImageConverter
+import os
 
 FILE_FORMAT = ['PNG', 'JPG', 'JPEG', 'ICO', 'WEBP']
-UPLOAD_FOLDER = '/'
+UPLOAD_FOLDER = './app/uploads/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -12,18 +15,36 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def main():
     app.logger.info("Loading home page... ")
-    return render_template('index.html', file_format=FILE_FORMAT)
+    return render_template('index.html', file_format=FILE_FORMAT,  output_file=None)
 
 @app.route('/home', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def convert():
     if request.method == 'POST':
         app.logger.info("The file converter function action... ")
+        
+        # Get the form values
         file_path = request.files['file-path']
         toConvert = request.form['file-format']
-        file_path.save()
-        return redirect(url_for('main'))
-    return render_template('index.html', file_format=FILE_FORMAT)
+
+        upload_file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file_path.filename))
+        print(upload_file_path)
+
+        # Save the file
+        file_path.save(upload_file_path)
+
+        # Converter Porcess
+        output_file = ImageConverter(upload_file_path, toConvert).convert()
+        list_val = output_file.split('/')
+        list_val.pop(0)
+        output_file = "/".join(list_val)
+
+        return render_template('index.html', file_format=FILE_FORMAT,  output_file=output_file)
+    return redirect(url_for('main'))
+
+@app.route('/app/uploads/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
